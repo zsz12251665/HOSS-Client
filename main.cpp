0499ba5c5dc1d1f09ccbbc8473936927dbd5ea9b
whaohan <2778351280@qq.com>
2020-06-01 18:02:41 +0800

    the initial version user interface of homework

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include "airplanner.h"
 #include <QApplication>
 #include "choice.h"
+#include "homework.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Airplanner w;
+    Airplanner userDefined;
+    Homework homework;
 
-   Choice dlg;
-       if (dlg.exec() == QDialog::Accepted)
-       {
-          w.show();
-          return a.exec();
-       }
-       else return 0;
+    Choice dlg;
+    if (dlg.exec() == QDialog::Accepted)
+    {
+       userDefined.show();
+       return a.exec();
+    }
+    else if(dlg.exec() == QDialog::Rejected)
+    {
+        homework.show();
+        return a.exec();
+    }
+    else return 0;
+
 }
